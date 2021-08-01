df4bf14e49b6bfa474fd92481d51771e3973c531
dns147 <74856237+dns147@users.noreply.github.com>
2021-08-02 00:07:38 +0300

    homework-6

--- src/client/ClientWorld.js
+++ src/client/ClientWorld.js
@@ -1,9 +1,10 @@
 import PositionedObject from '../common/PositionedObject';
 import sprites from '../configs/sprites';
 import ClientCell from './ClientCell';
+import { clamp } from '../common/util';
 
 class ClientWorld extends PositionedObject {
-  constructor(game, engine, levelCfg) {
+  constructor(game, engine, levelCfg, namePlayer) {
     super();
 
     const worldHeight = levelCfg.map.length;
@@ -14,6 +15,7 @@ class ClientWorld extends PositionedObject {
       game,
       engine,
       levelCfg,
+      namePlayer, //---имя игрока---
       height: worldHeight * cellSize,
       width: worldWidth * cellSize,
       worldHeight,
@@ -26,7 +28,7 @@ class ClientWorld extends PositionedObject {
 
   // --- Отрисовка карты через метод renderSpriteFrame класса ClientEngine ---
   init() {
-    const { levelCfg, map, worldWidth, worldHeight } = this;
+    const { levelCfg, map, worldWidth, worldHeight, namePlayer } = this;
 
     for (let row = 0; row < worldHeight; row++) {
       for (let col = 0; col < worldWidth; col++) {
@@ -39,6 +41,7 @@ class ClientWorld extends PositionedObject {
           cellCol: col,
           cellRow: row,
           cellCfg: levelCfg.map[row][col],
+          namePlayer: namePlayer, //---имя игрока---
         });
       }
     }
@@ -49,17 +52,83 @@ class ClientWorld extends PositionedObject {
     const { levelCfg, map, worldWidth, worldHeight } = this;
 
     for (let layerId = 0; layerId < levelCfg.layers.length; layerId++) {
-      for (let row = 0; row < worldHeight; row++) {
-        for (let col = 0; col < worldWidth; col++) {
-          map[row][col].render(time, layerId);
-        }
+      const layer = levelCfg.layers[layerId];
+
+      if (layer.isStatic) {
+        this.renderStaticLayer(time, layer, layerId);
+      } else {
+        this.renderDynamicLayer(time, layerId, this.getRenderRange());
       }
     }
   }
 
-  // --- Берём ячейку мира и возвращаем её ---
+  //--- метод статической отрисовки слоёв ---
+  renderStaticLayer(time, layer, layerId) {
+    const { engine } = this;
+    const { camera } = engine;
+
+    const layerName = 'static_layer_' + layerId;
+    const cameraPos = camera.worldBounds();
+
+    if (!layer.isRendered) {
+      engine.addCanvas(layerName, this.width, this.height);
+      engine.switchCanvas(layerName);
+
+      camera.moveTo(0, 0, false);
+
+      this.renderDynamicLayer(time, layerId);
+
+      camera.moveTo(cameraPos.x, cameraPos.y, false);
+
+      engine.switchCanvas('main');
+      layer.isRendered = true;
+    }
+
+    engine.renderCanvas(layerName, cameraPos, { x: 0, y: 0, width: cameraPos.width, height: cameraPos.height });
+  }
+
+  //--- метод динамической отрисовки слоёв ---
+  renderDynamicLayer(time, layerId, rangeCells) {
+    const { map, worldWidth, worldHeight } = this;
+
+    if (!rangeCells) {
+      rangeCells = {
+        startCell: this.cellAt(0, 0),
+        endCell: this.cellAt(worldWidth - 1, worldHeight - 1),
+      };
+    }
+
+    const { startCell, endCell } = rangeCells;
+
+    for (let row = startCell.row; row <= endCell.row; row++) {
+      for (let col = startCell.col; col <= endCell.col; col++) {
+        map[row][col].render(time, layerId);
+      }
+    }
+  }
+
+  // ---  Берём ячейку мира и возвращаем её ---
   cellAt(col, row) {
     return this.map[row] && this.map[row][col];
+  }
+
+  //--- Находим ячейку по координатам в мире---
+  cellAtXY(x, y) {
+    const { width, height, cellWidth, cellHeight } = this;
+
+    //---получаем ячейку в координатах колонка строка---
+    return this.cellAt((clamp(x, 0, width - 1) / cellWidth) | 0, (clamp(y, 0, height - 1) / cellHeight) | 0);
+  }
+
+  //--- Возвращает ячейки с какой по какую надо рендерить ---
+  getRenderRange() {
+    const { x, y, width, height } = this.engine.camera.worldBounds();
+    const { cellWidth, cellHeight } = this;
+
+    return {
+      startCell: this.cellAtXY(x - cellWidth, y - cellHeight),
+      endCell: this.cellAtXY(x + width + cellWidth, y + height + cellHeight),
+    };
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,88 +1,19 @@
 import './index.scss';
-import SenseiWalk from './assets/Male-4-Walk.png';
 import ClientGame from './client/ClientGame';
 
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
-const spriteW = 48;
-const spriteH = 48;
-const shots = 3;
-let cycle = 0;
-let bottomPressed = null;
-let pX = (canvas.width - spriteW) / 2;
-let pY = (canvas.height - spriteH) / 2;
+const inputNamePlayer = document.getElementById('name');
+const button = document.querySelector('.btn');
+const enterRoom = document.querySelector('.start-game');
 
-const img = document.createElement('img');
-img.src = SenseiWalk;
-
-document.addEventListener('keydown', (e) => {
-  bottomPressed = e.code;
-});
-
-document.addEventListener('keyup', () => {
-  bottomPressed = null;
-});
-
-function renderPlayer(n) {
-  cycle = (cycle + 1) % shots;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.drawImage(img, cycle * spriteW, n * 48, spriteW, spriteH, pX, pY, 48, 48);
+if (!inputNamePlayer.value) {
+  button.disabled = true;
 }
 
-function walk(timestamp) {
-  switch (bottomPressed) {
-    case 'Down':
-    case 'ArrowDown':
-      pY += 10;
-      renderPlayer(0);
-      break;
-
-    case 'Left':
-    case 'ArrowLeft':
-      pX -= 10;
-      renderPlayer(1);
-      break;
-
-    case 'Right':
-    case 'ArrowRight':
-      pX += 10;
-      renderPlayer(2);
-      break;
-
-    case 'Up':
-    case 'ArrowUp':
-      pY -= 10;
-      renderPlayer(3);
-      break;
-
-    default:
-      bottomPressed = null;
-  }
-
-  if (pX < 0) {
-    pX = 0;
-  }
-
-  if (pX > canvas.width - spriteW) {
-    pX = canvas.width - spriteW;
-  }
-
-  if (pY < 0) {
-    pY = 0;
-  }
-
-  if (pY > canvas.height - spriteH) {
-    pY = canvas.height - spriteH;
-  }
-
-  window.requestAnimationFrame(walk);
-}
-
-img.addEventListener('load', () => {
-  ctx.drawImage(img, spriteW, 0, spriteW, spriteH, pX, pY, 48, 48);
-  window.requestAnimationFrame(walk);
+inputNamePlayer.addEventListener('input', () => {
+  button.disabled = false;
 });
 
-window.addEventListener('load', () => {
-  ClientGame.init({ tagId: 'game' });
+button.addEventListener('click', () => {
+  enterRoom.remove();
+  ClientGame.init({ tagId: 'game', namePlayer: inputNamePlayer.value });
 });
